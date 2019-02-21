ceb491b8c91eca78f35bdd8a0588a48e58524e44
keck <keck@keck6cateorg.cate.org>
2019-02-21 09:55:47 -0800

    more stuff

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -47,9 +47,28 @@ class TodoListViewController: UITableViewController {
         
         
     }
-
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the Add Item button on our UIAlert
+            print("Success!")
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
     
 }
-    
 
 
